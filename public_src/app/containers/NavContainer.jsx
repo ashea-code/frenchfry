1dfe510c8b05cebf49dcdad3c1ff75c2b5ac32e0
AaronShea <aaron@aaronshea.me>
2019-07-15 09:03:18 -0700

    update nav container

--- public_src/app/containers/NavContainer.jsx
+++ public_src/app/containers/NavContainer.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
 import 'tabler-react/dist/Tabler.css';
+import './NavContainer.css';
 
 import { Nav, Site } from 'tabler-react';
 
 const NavContainer = () => (
   <Site.Header>
-    <Nav>
+    <Nav className="full-nav-header">
       <Nav.Item icon="bar-chart" href="/admin">Dashboard</Nav.Item>
       <Nav.Item icon="layers" href="/admin/posts">Posts</Nav.Item>
       <Nav.Item icon="tag" href="/admin/tags">Tags</Nav.Item>
       <Nav.Item icon="book" href="/admin/collections">Collections</Nav.Item>
       <Nav.Item icon="user" href="/admin/users">Users</Nav.Item>
-      <Nav.Item icon="logout" href="/api/auth/logout">
+      <Nav.Item icon="log-out" href="/api/auth/logout">
         Logout
       </Nav.Item>
     </Nav>
